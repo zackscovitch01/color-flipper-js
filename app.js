9f8cbc1a2b44b4b8f5801223ffdb0944d6567d1b
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-04-17 19:57:48 -0400

    simple-color-flipper

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  let randomColor = colors[Math.floor(Math.random() * colors.length)];
+  color.textContent = randomColor;
+  document.body.style.backgroundColor = randomColor;
+});
